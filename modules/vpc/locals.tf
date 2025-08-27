273caea54cbbf3a2a8bcde86eba2bd248dc5637d
YangMingHao <yangmh@anchnet.com>
2025-08-27 16:37:18 +0800

    workflow 12

--- modules/vpc/locals.tf
+++ modules/vpc/locals.tf
@@ -1,6 +1,6 @@
 locals {
   # 动态读取配置文件 - 根据环境变量选择对应的配置文件
-  vpc_config = yamldecode(file("../../env/${var.environment}/lz-core/vpc.yaml"))
+  vpc_config = yamldecode(file("../env/${var.environment}/lz-core/vpc.yaml"))
 
   # 区域配置
   region = try(local.vpc_config["region_info"]["region"], "ap-guangzhou")
